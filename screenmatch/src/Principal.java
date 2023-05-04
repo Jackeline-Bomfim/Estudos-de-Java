64de4524cc51bb27957058253c57ccc172e8ca87
Jackeline Bomfim <62723813+Jackeline-Bomfim@users.noreply.github.com>
2023-05-03 21:57:10 -0300

    Add files via upload

---
+++ screenmatch/src/Principal.java
@@ -0,0 +1,63 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+
+
+
+public class Principal {
+    public static void main(String[] args) {
+
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+
+        meuFilme.exibeFichaTecnica();
+
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        System.out.println(meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.getSomaAvaliacoes());
+        System.out.println(meuFilme.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2002);
+
+        System.out.println(lost.getNome());
+        System.out.println(lost.getAnoDeLancamento());
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpsodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+
+        System.out.println("Duração em minutos de Lost: " + lost.getDuracaoEmMinutos()
+        );
+
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("Avatar");
+        outroFilme.setAnoDeLancamento(2023);
+        outroFilme.setDuracaoEmMinutos(200);;
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(outroFilme);
+        calculadora.inclui(lost);
+        System.out.println(calculadora.getTempoTotal());
+
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        filtro.filtra(meuFilme);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(lost);
+        episodio.setTotalDeVisualizacoes(300);
+
+        filtro.filtra(episodio);
+
+
+    }
+}
